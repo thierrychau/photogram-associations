3a9c7e65eb7bc65cb546cc5e70e41b684399a717
Thierry Chau <thierry.chau@gmail.com>
2023-10-05 16:23:16 +0000

    most  association accessors working

--- app/models/follow_request.rb
+++ app/models/follow_request.rb
@@ -22,8 +22,10 @@ class FollowRequest < ApplicationRecord
   ## Direct associations
 
   # FollowRequest#sender: returns a row from the users table associated to this follow request by the sender_id column
+  belongs_to(:sender, :class_name => "User", :foreign_key => "sender_id")
 
   # FollowRequest#recipient: returns a row from the users table associated to this follow request by the recipient_id column
+  belongs_to(:recipient, :class_name => "User", :foreign_key => "recipient_id")
 
   def sender
     my_sender_id = self.sender_id
